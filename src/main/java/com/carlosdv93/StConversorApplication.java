8f4e43b845ce6c8b9af627d337defdd4799d329a
= <carlos9_cv@hotmail.com>
2019-08-20 21:45:25 -0300

    First Commit

---
+++ src/main/java/com/carlosdv93/StConversorApplication.java
@@ -0,0 +1,13 @@
+package com.carlosdv93;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StConversorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StConversorApplication.class, args);
+	}
+
+}
