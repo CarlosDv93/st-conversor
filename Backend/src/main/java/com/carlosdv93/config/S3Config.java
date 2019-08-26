79e92e2f5d9f1957127864f8dd9ee26088d6fe53
= <carlos9_cv@hotmail.com>
2019-08-26 20:56:05 -0300

    Apresentação do REST e configuração de upload ao S3

---
+++ Backend/src/main/java/com/carlosdv93/config/S3Config.java
@@ -0,0 +1,49 @@
+package com.carlosdv93.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+@Component
+@PropertySource("aws.config.properties")
+public class S3Config {
+
+	@Value("${aws.config.access_key_id}")
+	private String awsId;
+
+	@Value("${aws.config.secret_access_key}")
+	private String awsKey;
+
+	@Value("${aws.config.s3.region}")
+	private String region;
+	
+	@Bean
+	public AmazonS3 s3client() {
+		BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsId, awsKey);
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
+				.withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+
+		return s3Client;
+	}
+
+	/*
+	 * private String access_key_id; private String secret_access_key; private
+	 * String s3_region;
+	 * 
+	 * @Bean public AmazonS3 s3client() { BasicAWSCredentials awsCreds = new
+	 * BasicAWSCredentials(access_key_id, secret_access_key); AmazonS3 s3Client =
+	 * AmazonS3ClientBuilder.standard() .withRegion(Regions.fromName(s3_region))
+	 * .withCredentials(new AWSStaticCredentialsProvider(awsCreds)) .build();
+	 * 
+	 * return s3Client; }
+	 */
+}
---
+++ Backend/src/main/java/com/carlosdv93/services/S3Service.java
@@ -0,0 +1,61 @@
+package com.carlosdv93.services;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
+@Service
+@PropertySource("aws.config.properties")
+public class S3Service {
+
+	private Logger log = org.slf4j.LoggerFactory.getLogger(S3Service.class);
+
+	@Autowired
+	private AmazonS3 s3client;
+
+	@Value("${aws.config.s3.bucket}")
+	private String bucketName;
+
+	public URI uploadFile(MultipartFile multipartFile) throws IOException, URISyntaxException {
+		try {
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream is;
+			is = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+
+			return uploadFile(is, fileName, contentType);
+		} catch (IOException e) {
+			throw new IOException("Erro de IO: " + e.getMessage());
+		}
+
+	}
+
+	public URI uploadFile(InputStream is, String fileName, String contentType) throws URISyntaxException {
+		try {
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentType(contentType);
+			log.info("Iniciando Upload........");
+			s3client.putObject(bucketName, fileName, is, meta);
+			log.info(fileName);
+			log.info(contentType);
+			log.info("Upload Finalizado!!!");
+
+			return s3client.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new URISyntaxException("Erro ao converter URL para URI", contentType);
+		}
+
+	}
+
+}
